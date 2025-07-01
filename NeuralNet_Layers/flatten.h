af30a6c1fa78af19fd287163e57192d29153d4e2
Irene Spanou <2804507+iresil@users.noreply.github.com>
2025-07-01 21:25:52 +0300

    Added a Flatten layer, to translate inputs to 1D arrays

---
+++ NeuralNet_Layers/flatten.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../NeuralNet_Core/tensor.h"
+#include "../NeuralNet_Core/module.h"
+
+class Flatten : public Module
+{
+    public:
+        std::shared_ptr<Tensor> forward(std::shared_ptr<Tensor> input) override;
+};
