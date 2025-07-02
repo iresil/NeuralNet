256d0e154d740ffd9e88bde1e4732fde8635493d
Irene Spanou <2804507+iresil@users.noreply.github.com>
2025-07-02 22:02:17 +0300

    Added a Linear layer, to perform learnable affine transformations

---
+++ NeuralNet_Layers/linear.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../NeuralNet_Core/module.h"
+#include "../NeuralNet_Core/tensor.h"
+
+class Linear : public Module
+{
+    private:
+        std::shared_ptr<Tensor> _weight;
+        std::shared_ptr<Tensor> _bias;
+        std::size_t _in_features;
+        std::size_t _out_features;
+        std::size_t _seed;
+
+    public:
+        Linear(std::size_t in_features, std::size_t out_features, std::size_t seed = 7);
+        std::shared_ptr<Tensor> forward(std::shared_ptr<Tensor> input) override;
+        void reset_parameters();
+};
