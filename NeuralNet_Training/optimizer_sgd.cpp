6c0da40966eb9678fd3eb0e9a1111124edfc4b3d
Irene Spanou <2804507+iresil@users.noreply.github.com>
2025-07-11 21:57:22 +0300

    Added optimizer to handle error minimization
    Uses Stochastic Gradient Descent as an optimization algorithm

---
+++ NeuralNet_Training/optimizer_sgd.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "optimizer_sgd.h"
+#include "../NeuralNet_Core/tensor.h"
+
+SGD::SGD(std::vector<std::pair<std::string, std::shared_ptr<Tensor>>> params, float lr) :
+    _params(params), _learning_rate(lr) { }
+
+void SGD::step()
+{
+    for (auto &param : _params)
+    {
+        for (std::size_t i = 0; i < param.second->count(); i++)
+        {
+            param.second->data()[i] -= _learning_rate * param.second->grad()[i];
+        }
+    }
+}
+
+void SGD::zero_grad()
+{
+    for (auto &param : _params)
+    {
+        param.second->zero_grad();
+    }
+}
